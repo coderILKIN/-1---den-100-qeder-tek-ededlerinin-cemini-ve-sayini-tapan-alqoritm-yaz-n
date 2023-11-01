d3a8cd2f42c525340fdcb28bcf1a5e598c29b41d
coderILKIN <tu758403c@code.edu.az>
2023-11-01 15:56:31 +0400

    Numbers total aded

---
+++ Ededlerin cemi/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Ededlerin_cemi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. 1 - den 100 qeder tek ededlerinin cemini tapan alqoritm yazın flowchartda.
+
+            //int total = 0;
+
+            //for (int i = 1; i<100; i++)
+            //{
+            //    if (i%2==1)
+            //    {
+            //        total += i;
+            //    }
+            //}
+
+            //Console.WriteLine(total);
+
+
+
+            //2. 1 - den 100 qeder cut ededlerin sayini tapan alqoritm yazın flowchartda.
+
+            //int totalCount = 0;
+
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        totalCount++;
+            //    }
+            //}
+            //Console.WriteLine(totalCount);
+
+
+            //3.Daxil olunan ededin 2 - nin quvveti olub olmadigini tapin flowchartda.
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Ededi daxil edin...");
+            //    int eded = Convert.ToInt32(Console.ReadLine());
+            //    int i = eded;
+            //    int ust = 0;
+            //    do
+            //    {
+            //        if (i % 2 == 0)
+            //        {
+            //            i = i / 2;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine(eded.ToString() + " 2-nin quvveti deyil...");
+            //            break;
+            //        }
+            //        ust++;
+            //    } while (i != 1);
+            //    Console.WriteLine(eded.ToString() + " = 2 ^ " + ust.ToString());
+            //}
+
+
+            //infiniti loop
+
+            //int i = 0;
+
+            //while (true)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //    if (i > 30)
+            //    {
+            //        break;
+            //    }
+            //}
+
+
+           // infiniti loop
+
+            //for (int i = 1; i > 0; i++)
+            //{
+            //    Console.WriteLine(i);
+
+            //    if (i>=100)
+            //    {
+            //        break;
+            //    }
+            //}
+
+
+        }
+    }
+}
